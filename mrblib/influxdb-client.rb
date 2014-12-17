a377ab079284b4e1c9e74d44134f809b035d62a8
inokappa <inokara@gmail.com>
2014-12-17 09:05:39 +0900

    modified

--- mrblib/influxdb-client.rb
+++ mrblib/influxdb-client.rb
@@ -11,10 +11,20 @@ class Influxdb
         'User-Agent'     => @ua,
       }
     end
+
+    def http_request
+      HttpRequest.new()
+    end
+
     def post(data)
       request_url = "#{@url}/db/#{@db}/series?u=#{@user}&p=#{@pass}"
-      http = HttpRequest.new()
-      http.post(request_url, JSON::stringify(data), @request)
+      http_request.post(request_url, JSON::stringify(data), @request)
+    end
+
+    def get_metrics(query)
+      query = HTTP::URL::encode(query)
+      request_url = "#{@url}/db/#{@db}/series?q=#{query}&u=#{@user}&p=#{@pass}"
+      http_request.get(request_url, nil, @request)
     end
   end
 end
